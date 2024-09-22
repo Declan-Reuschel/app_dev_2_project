7e755b91fe22418afbd4f5386b6950a3cb66e088
Declan-Reuschel-KSU <dreusche@students.kennesaw.edu>
2024-09-22 08:13:14 -0400

    Initial Java files

--- assignment_1/Encryption.java
+++ assignment_1/Encryption.java
@@ -1,1 +1,46 @@
+package assignment_1;
 
+import java.util.ArrayList;
+
+
+class Encryption {
+
+
+    ArrayList<Character> symbols = new ArrayList<Character>();
+    ArrayList<Character> alphabets = new ArrayList<Character>();
+
+    public Encryption() {
+
+    symbols.add('!');
+    symbols.add('@');
+    symbols.add('#');
+    symbols.add('$');
+    symbols.add('^');
+    symbols.add('&');
+    symbols.add('*');
+    symbols.add('(');
+    symbols.add(')');
+    symbols.add('_');
+    symbols.add('-');
+    symbols.add('+');
+    symbols.add('=');
+    symbols.add('?');
+    symbols.add(',');
+    symbols.add('{');
+    symbols.add('}');
+    symbols.add('[');
+    symbols.add(']');
+    symbols.add('/');
+    symbols.add('|');
+    symbols.add(';');
+    symbols.add(':');
+    symbols.add('.');
+    symbols.add('<');
+    symbols.add('>');
+
+    for(char letter = 'a'; letter <= 'z'; letter++) {
+        alphabets.add(letter);
+        }
+    }
+
+}
---
+++ assignment_1/EncryptionTest.java
@@ -0,0 +1,14 @@
+package assignment_1;
+
+public class EncryptionTest {
+    
+    // driver class
+
+    // main method; add test function calls here
+    public static void main(String[] args) {
+        
+    }
+
+    // define supporting methods for testing/driver class here
+
+}
