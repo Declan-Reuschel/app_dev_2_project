a71fddda9dced3c1f37ef417b78e01fb7cb37b38
prev3 <prevenna@students.kennesaw.edu>
2024-10-06 13:50:31 -0400

    Fix formatting

--- assignment_1/Encryption.java
+++ assignment_1/Encryption.java
@@ -4,84 +4,70 @@ import java.util.ArrayList;
 
 //Creation of Java ArrayLists
 class Encryption {
-
-
     ArrayList<Character> symbols = new ArrayList<Character>();
     ArrayList<Character> alphabets = new ArrayList<Character>();
 
     public Encryption() {
+        symbols.add('!');
+        symbols.add('@');
+        symbols.add('#');
+        symbols.add('$');
+        symbols.add('^');
+        symbols.add('&');
+        symbols.add('*');
+        symbols.add('(');
+        symbols.add(')');
+        symbols.add('_');
+        symbols.add('-');
+        symbols.add('+');
+        symbols.add('=');
+        symbols.add('?');
+        symbols.add(',');
+        symbols.add('{');
+        symbols.add('}');
+        symbols.add('[');
+        symbols.add(']');
+        symbols.add('/');
+        symbols.add('|');
+        symbols.add(';');
+        symbols.add(':');
+        symbols.add('.');
+        symbols.add('<');
+        symbols.add('>');
 
-    symbols.add('!');
-    symbols.add('@');
-    symbols.add('#');
-    symbols.add('$');
-    symbols.add('^');
-    symbols.add('&');
-    symbols.add('*');
-    symbols.add('(');
-    symbols.add(')');
-    symbols.add('_');
-    symbols.add('-');
-    symbols.add('+');
-    symbols.add('=');
-    symbols.add('?');
-    symbols.add(',');
-    symbols.add('{');
-    symbols.add('}');
-    symbols.add('[');
-    symbols.add(']');
-    symbols.add('/');
-    symbols.add('|');
-    symbols.add(';');
-    symbols.add(':');
-    symbols.add('.');
-    symbols.add('<');
-    symbols.add('>');
-
-    for(char letter = 'a'; letter <= 'z'; letter++) {
-        alphabets.add(letter);
+        for(char letter = 'a'; letter <= 'z'; letter++) {
+            alphabets.add(letter);
         }
     }
-//add a method which takes an integer and returns the alphabet stored at that position 
-    
+
+    //add a method which takes an integer and returns the alphabet stored at that position    
     public char getChar(int par1Int) {
-        
         return alphabets.get(par1Int);
-
     }
-//add a method which takes in an alphabetic character and returns the index of the characterin the alphabets list
+
+    //add a method which takes in an alphabetic character and returns the index of the characterin the alphabets list
     public int getCharIndex(char par1Char) {
-        
         for (int i = 0; i < alphabets.size(); i++) {
-            
             if (alphabets.get(i) == par1Char) return i;
-
-          }
-
-    return -1;
-
+        }
+        return -1;
     }
-//add a method which takes an integer and returns the symbol stored at that position 
+
+    //add a method which takes an integer and returns the symbol stored at that position 
     public char getSymbol(int par1Int) {
-        
         return symbols.get(par1Int);
-
     }
-// add a method which takes in a symbol and returns the index of the symbol in the symbols list
+    // add a method which takes in a symbol and returns the index of the symbol in the symbols list
     public int getSymbolIndex(char par1Char) {
-        
         for (int i = 0; i < symbols.size(); i++) {
-            
             if (symbols.get(i) == par1Char) return i;
-
-          }
-
-    return -1;
-
+        }
+        return -1;
     }
-// add a method which takes in a plain-text string and returns the encrypted version of that string 
+
+    // add a method which takes in a plain-text string and returns the encrypted version of that string 
     public String encryptString(String par1String) {
-//method which takes plain-text string to lowercase 
+        //method which takes plain-text string to lowercase 
         String str = par1String.toLowerCase();
         String encrypt_str = "";
 
@@ -99,15 +85,12 @@ class Encryption {
                 str_br.append(getSymbol(char_index));
             }
             else return "Error: Invalid Character";
-//if an invalid alphabet character is found, return the error string 
-          }
-
+            //if an invalid alphabet character is found, return the error string 
+        }
         return encrypt_str;
-
     }
-// adda method which takes in an encrypted string and returns the decrypted version of that string 
+    // adda method which takes in an encrypted string and returns the decrypted version of that string 
     public String decryptString(String par1String) {
-
         String str = par1String.toLowerCase();
         String decrypt_str = "";
 
@@ -118,18 +101,13 @@ class Encryption {
         char[] tmpArray = str.toCharArray();
 
         for (int i = 0; i < tmpArray.length; i++) {
-            
             int symbol_index = getSymbolIndex(tmpArray[i]);
 
             if (symbol_index != -1) {
                 str_br.append(getChar(symbol_index));
             }
             else return "Error: Invalid Symbol";
-
           }
-
         return decrypt_str;
-
     }
-
 }
--- assignment_1/EncryptionTest.java
+++ assignment_1/EncryptionTest.java
@@ -5,18 +5,19 @@ public class EncryptionTest {
     // driver class
 
     // main method; add test function calls here
-    public static void Encryption(){
+    public static void Encryption() {
         System.out.println("1 - Encrypt%\n");
         System.out.println("2 - Decrypt%\n");
         System.out.println("3 - Exit");
-        }
-        public static void main(String[] args) {
+    }
+
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         Encryption();
-            System.out.println("Please enter choice: ");
+        System.out.println("Please enter choice: ");
 
-            // please feel free to edit if i messed up anything so far lol - amanda 
+        // please feel free to edit if i messed up anything so far lol - amanda 
     }
 
     // define supporting methods for testing/driver class here
