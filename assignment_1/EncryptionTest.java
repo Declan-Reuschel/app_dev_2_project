e7592bf9c0b3d74c4eedf580e6388ca2a176b587
prev3 <prevenna@students.kennesaw.edu>
2024-10-06 14:02:47 -0400

    Fix prompt and allow it to run multiple times

--- assignment_1/EncryptionTest.java
+++ assignment_1/EncryptionTest.java
@@ -2,17 +2,30 @@ package assignment_1;
 import java.util.Scanner;
 
 public class EncryptionTest {
-    public static void Encryption() {
-        System.out.println("1 - Encrypt%\n");
-        System.out.println("2 - Decrypt%\n");
-        System.out.println("3 - Exit");
-    }
-
     // main method; add test function calls here
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner stdin = new Scanner(System.in);
 
-        Encryption();
-        System.out.println("Please enter choice: ");
+        encryption_loop: while (true) {
+            System.out.println("1 - Encrypt");
+            System.out.println("2 - Decrypt");
+            System.out.println("3 - Exit");
+            System.out.println();
+            System.out.println("Enter choice: ");
+            String user_response = stdin.nextLine();
+
+            switch(user_response) {
+                case "1":
+                    System.out.println("hit 1");
+                    break;
+                case "2":
+                    System.out.println("hit 2");
+                    break;
+                case "3":
+                    break encryption_loop;
+                default:
+                    System.out.println("Error: Please enter valid input\n");
+            }
+        }
     }
 }
