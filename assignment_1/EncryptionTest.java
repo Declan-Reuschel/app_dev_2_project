0576d8aa6d3b918dc82f8a8b328c3dc6c6f107b9
prev3 <prevenna@students.kennesaw.edu>
2024-10-06 14:14:57 -0400

    Add encryption to tester

--- assignment_1/EncryptionTest.java
+++ assignment_1/EncryptionTest.java
@@ -5,6 +5,7 @@ public class EncryptionTest {
     // main method; add test function calls here
     public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
+        Encryption encryptor = new Encryption();
 
         encryption_loop: while (true) {
             System.out.println("1 - Encrypt");
@@ -16,11 +17,16 @@ public class EncryptionTest {
 
             switch(user_response) {
                 case "1":
-                    System.out.println("hit 1");
-                    break;
+                    System.out.println("Enter the plain text message: ");
+                    String plain_text_message = stdin.nextLine();
+                    System.out.println();
+                    System.out.println("Encrypted Msg: " + encryptor.encryptString(plain_text_message));
+                    break encryption_loop;
                 case "2":
-                    System.out.println("hit 2");
-                    break;
+                    System.out.println("Enter the encrypted message: ");
+                    String encrypted_message = stdin.nextLine();
+                    System.out.println(encryptor.decryptString(encrypted_message));
+                    break encryption_loop;
                 case "3":
                     break encryption_loop;
                 default:
