02e3bc4c75a5c3fa5f834e9862286b5a7369bc4e
prev3 <prevenna@students.kennesaw.edu>
2024-10-06 13:57:25 -0400

    Rearrange and remove some comments

--- assignment_1/EncryptionTest.java
+++ assignment_1/EncryptionTest.java
@@ -2,22 +2,17 @@ package assignment_1;
 import java.util.Scanner;
 
 public class EncryptionTest {
-    // driver class
-
-    // main method; add test function calls here
     public static void Encryption() {
         System.out.println("1 - Encrypt%\n");
         System.out.println("2 - Decrypt%\n");
         System.out.println("3 - Exit");
     }
 
+    // main method; add test function calls here
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         Encryption();
         System.out.println("Please enter choice: ");
     }
-
-    // define supporting methods for testing/driver class here
-
 }
