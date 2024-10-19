4d1000cd8a769894912c86bdb2439afc4ba0ba39
Amanda R. <142240337+arajda@users.noreply.github.com>
2024-10-19 14:54:01 -0400

    Update insurance.java

--- insurance.java
+++ insurance.java
@@ -1,7 +1,10 @@
-import java.util.ArrayList;
+public abstract class Insurance implements deductible {
+  //policy holder name 
+  private String policyholderName;
+  //unique policy number 
+  private int policyNumber;
+  // total number of policies created so far , initialized to 0
+  private static int totalPolicies = 0;
+  // premium amount, initialized to 0 
+  private double premiumAmount = 0;
 
-public class insurance {
-  public static void (String[] args){
-    
-}
-}
