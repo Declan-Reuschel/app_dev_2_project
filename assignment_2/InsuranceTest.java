54c1873147327eed57004e1ee3eb32d114a0bdd7
Amanda R. <142240337+arajda@users.noreply.github.com>
2024-10-21 13:43:57 -0400

    Update InsuranceTest.java

--- assignment_2/InsuranceTest.java
+++ assignment_2/InsuranceTest.java
@@ -21,11 +21,47 @@ public class InsuranceTest {
 
             switch(user_response) {
                 case "1":
+                    
+                    System.out.println("Enter name of policy holder: ");
+                    String par1PolicyHolder = stdin.nextLine();
+                    System.out.println("Enter deductible amount: ");
+                    String par2DeductibleAmount= stdin.nextLine();
+                    System.out.println("Enter co-payment: ");
+                    double par3Copay = stdin.nextDouble();
+                    System.out.println("Enter total out-of-pocket amount: ");
+                    double par4OutOfPocket = stdin.nextDouble();
+
+                    System.out.println("1 - Create Health Insurance Policy");
+                    System.out.println("2 - Create Term-Life Insurance Policy");
+                    System.out.println("3 - Exit");
+                    System.out.println();
+                    System.out.println("Enter choice: ");
+            
                     break encryption_loop;
+                    
                 case "2":
+                    
+                    System.out.println("Enter name of policy holder: ");
+                    par1PolicyHolder = stdin.nextLine();
+                    System.out.println("Enter name of beneficiary: ");
+                    String par2Beneficiary = stdin.nextLine();
+                    System.out.println("Enter number of years in term: ");
+                    int par3Term = stdin.nextInt();
+                    System.out.println("Enter amount of payout: ");
+                    double par4Payout = stdin.nextDouble();
+
+                    System.out.println("1 - Create Health Insurance Policy");
+                    System.out.println("2 - Create Term-Life Insurance Policy");
+                    System.out.println("3 - Exit");
+                    System.out.println();
+                    System.out.println("Enter choice: ");
+                    
                     break encryption_loop;
+                    
                 case "3":
+                    
                     break encryption_loop;
+                    
                 default:
                     System.out.println("Error: Please enter valid input\n");
             }
