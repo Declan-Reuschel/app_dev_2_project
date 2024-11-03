2267fde2e98145e0ace9cfc20f5d2e156154af46
Amanda R. <142240337+arajda@users.noreply.github.com>
2024-11-03 10:06:10 -0500

    added comments

--- assignment_3/Checking.java
+++ assignment_3/Checking.java
@@ -1,11 +1,11 @@
 package assignment_3;
-
+//inherits bank account class
 public class Checking extends BankAccount {
 
 
-
+//double, private daily limit 
     private double dailyWithdrawalLimit;
-
+//overloaded constructor 
     public Checking(String par1Name, String par2UserID, String par3UserPassword) {
 
         super(par1Name, par2UserID, par3UserPassword);
