eb75e80691d6591c9589d8a0fa22149de316ef86
prev3 <prevenna@students.kennesaw.edu>
2024-11-04 13:02:28 -0500

    Fix spacing

--- assignment_3/BankExceptions.java
+++ assignment_3/BankExceptions.java
@@ -1,37 +1,27 @@
 package assignment_3;
 //four exception classes 
 //invalid password exception
- class InvalidPasswordFormatException extends Exception {
-
-     public InvalidPasswordFormatException(String par1ErrorMessage) {
-         super(par1ErrorMessage);
-     }
-
+class InvalidPasswordFormatException extends Exception {
+    public InvalidPasswordFormatException(String par1ErrorMessage) {
+        super(par1ErrorMessage);
+    }
 }
 //negative dollar amount exception 
 class NegativeDollarAmountException extends Exception {
-
-
     public NegativeDollarAmountException(String par1ErrorMessage) {
         super(par1ErrorMessage);
     }
-
 }
 //insufficient funds exception 
 class InsufficientFundsException extends Exception {
-
-
     public InsufficientFundsException(String par1ErrorMessage) {
         super(par1ErrorMessage);
     }
-
 }
 
 class CustomerAccountNotFoundException extends Exception {
-
-//customer account not found exception 
+    //customer account not found exception 
     public CustomerAccountNotFoundException(String par1ErrorMessage) {
         super(par1ErrorMessage);
     }
-
 }
