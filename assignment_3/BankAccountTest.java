de9f70fabe13de39bc753635714387f83edf1deb
prev3 <prevenna@students.kennesaw.edu>
2024-11-04 12:53:50 -0500

    Formatting and syntax fixes

--- assignment_3/BankAccountTest.java
+++ assignment_3/BankAccountTest.java
@@ -11,7 +11,6 @@ public class BankAccountTest {
 
     // process main menu, main method
     public static void main(String[] args) {
-
         banking_loop: while (true) {
             System.out.println("1 - Create An Account");
             System.out.println("2 - Delete An Account");
@@ -39,10 +38,9 @@ public class BankAccountTest {
                     try {
                         string password_val = checkPassword( n_user_password):
                             if (password_val != password.length() <8 || !password.contains("*")) throw new InvalidPasswordFormatException("Password is invalid."):
-                            }
-                    catch(InvalidPasswordFormatException invalidException){
-                        
-                        //not sure if i did this right please check over 
+                    } catch(InvalidPasswordFormatException invalidException) {
+
+                    }
 
                     Checking n_account = new Checking(n_customer_name, n_user_id, n_user_password);
                     bankAccounts.add(n_account);
@@ -50,7 +48,6 @@ public class BankAccountTest {
                     System.out.println("Account created! Returning to main menu...");
 
                     break;
-
                 case "2":
                     // delete a checking account 
                     System.out.println("Enter user ID: ");
@@ -59,7 +56,6 @@ public class BankAccountTest {
                     String d_user_password = stdin.nextLine();
 
                     try {
-
                         int d_account_index = searchUserAccount(d_user_id, d_user_password);
 
                         if (d_account_index == -1) throw new CustomerAccountNotFoundException("Account not found! Returning to main menu...");
@@ -69,27 +65,25 @@ public class BankAccountTest {
                         BankAccount.decrementAccounts();
 
                         System.out.println("Account deleted! Returning to main menu...");
-
                     } catch(CustomerAccountNotFoundException notFoundException) {
                         System.err.println(notFoundException.getMessage());
                     }
 
                     break;
-
                 case "3":
                     //make a deposit 
-
                     System.out.println("Option 3 - Make An Account Deposit...");
 
                     BankAccount a_account = promptForUserAccount();
                     if (a_account == null) break; // the exception handling itself is handled in promptForUserAccount
 
                     System.out.println("Account found - Enter dollar amount to deposit: ");
-                    try{
+                    try {
                         double a_deposit_amount= Double.parseDouble(stdin.nextLine());
                         if (a_depost_amount <= 0){
                             System.out.println("Deposit amount must be positive");
                         }
+                    } catch {}
                         //double check please 
                     // NEEDS ADDING VALIDATION FOR IF AMOUNT TO WITHDRAW IS POSITIVE, AND THROW EXCEPTION IF NEEDED
 
@@ -98,10 +92,8 @@ public class BankAccountTest {
                     System.out.println("Deposit made! Returning to main menu...");
 
                     break;
-
                 case "4":
                     //make a withdrawl 
-
                     System.out.println("Option 4 - Make An Account Withdrawal...");
 
                     BankAccount w_account = promptForUserAccount();
@@ -118,10 +110,8 @@ public class BankAccountTest {
                     System.out.println("Withdrawal made! Returning to main menu...");
 
                     break;
-
                 case "5":
                     //check account balance 
-
                     System.out.println("Option 5 - Check An Account Balance...");
 
                     BankAccount p_account = promptForUserAccount();
@@ -140,15 +130,11 @@ public class BankAccountTest {
                     System.out.println("=========================");
 
                     System.out.println("Information displayed. Returning to main menu...");
-
                     break;
                     //terminate program 
-
                 case "6":
                     System.out.println("Exiting program. Have a nice day!");
-
                     break banking_loop;
-
                 default:
                     System.out.println("Error: Please enter a valid choice (1 thru 6)\n");
             }
@@ -157,9 +143,7 @@ public class BankAccountTest {
 
 
     public static BankAccount promptForUserAccount() {
-
         try {
-
             System.out.println("Enter user ID: ");
             String g_user_id = stdin.nextLine();
             System.out.println("Enter password");
@@ -170,38 +154,26 @@ public class BankAccountTest {
             if (g_account_index == -1) throw new CustomerAccountNotFoundException("Account not found! Returning to main menu...");
 
             return bankAccounts.get(g_account_index);
-
         } catch(CustomerAccountNotFoundException notFoundException) {
             System.err.println(notFoundException.getMessage());
-
             return null;
         }
-
     }
 
     // returns -1 if not found; returns the index of the account in the arraylist if found
-
     public static int searchUserAccount(String par1UserID, String par2UserPassword) {
-
         int account_index = -1;
 
         for (int i = 0; i < bankAccounts.size(); i++) {
-
             BankAccount s_account = bankAccounts.get(i);
 
             if (s_account.getUserID().equals(par1UserID) && s_account.getUserPassword().equals(par2UserPassword)) {
-
                 account_index = i;
 
                 break;
             }
-
         }
 
-
         return account_index;
-
     }
-
-
 }
